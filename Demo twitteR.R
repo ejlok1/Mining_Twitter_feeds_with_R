5aaf9bf28bddd32675e78d8e423487c72b38f26c
Eu Jin Lok <ejlok1@hotmail.com>
2014-04-09 22:36:25 +1000

    try adding this

---
+++ Demo twitteR.R
@@ -0,0 +1,45 @@
+#Load the library
+library(twitteR)
+
+#####################################
+# Setup twitter access
+#####################################
+#ftp://cran.r-project.org/pub/R/web/packages/twitteR/twitteR.pdf
+
+# Reload the authorisation
+load("C:\\Users\\Eu\\Dropbox\\R GIT project\\doc\\Twitter Auth Credentials.Rdata")
+
+# get authorisation
+registerTwitterOAuth(cred)
+
+# Set up the certificate - required if run on Windows
+options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+
+##############################
+# Get Tweets about topic 
+##############################
+# search tweeter feeds based on users
+who <- "@MAS" 
+start <- '2014-03-20'
+end <- '2014-03-21'
+
+some_tweets <- searchTwitter(who,  since=start, until=end, n=3200)  #current limit of 3200 tweets 
+MAS.df <- twListToDF(some_tweets)  # convert to proper dataframe structure
+
+attributes(MAS.df$created)$tzone <- "Australia/sydney"
+MAS.df <- MAS.df[MAS.df$created >= as.POSIXct("2014-02-01 00:00:00",tz = "Australia/sydney"),]
+
+##############################
+# Get Tweets from user's official account 
+##############################
+# search tweeter feeds based on users
+who <- "@MAS" 
+some_tweets <- userTimeline(who, n=3200)  #current limit of 3200 tweets 
+MAS.df <- twListToDF(some_tweets)  
+
+attributes(MAS.df$created)$tzone <- "Australia/sydney"
+MAS.df <- MAS.df[MAS.df$created >= as.POSIXct("2014-02-01 00:00:00",tz = "Australia/sydney"),]
+
+
+
+
